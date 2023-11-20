5d892be8defd2955e78b0791e48b56c07d9276c0
locker55198 <150778940+locker55198@users.noreply.github.com>
2023-11-20 15:09:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register')
 def register():
     return render_template('register.html')
     
