41a4403e585163d8628ffb8b40aa43485a346edb
locker55198 <150778940+locker55198@users.noreply.github.com>
2023-11-23 16:08:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,31 @@ def favicon():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
- 
+         if request.method == 'POST':
+            name = request.form['name']
+
+           conn = get_db_connection()
+           cursor = conn.cursor()
+
+           sql_check = "SELECT * FROM facevote WHERE name = %s"
+           cursor.execute(sql_check, (name,))
+           result = cursor.fetchone()
+
+           if result:
+              cursor.close()
+              conn.close()
+              return redirect(url_for('register', error_message='Image already exists. Please choose a different image'))
+           else:
+             sql = "INSERT INTO facevote (name) VALUES (%s)"
+             cursor.execute(sql, (name,))
+             conn.commit()
+             cursor.close()
+             conn.close()
+             return redirect(url_for('index', success_message='Registration successful'))
+
+            cursor.close()
+            conn.close()
+    
          return render_template('register.html', success_message=request.args.get('success_message'), error_message=request.args.get('error_message'))
    
 @app.route('/login', methods=['GET', 'POST'])
