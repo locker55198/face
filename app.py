fc3e4abe3377d99611c3157b2171ff7cbb681ba9
locker55198 <150778940+locker55198@users.noreply.github.com>
2023-11-23 23:28:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -48,27 +48,43 @@ def register():
             return redirect(url_for('index', success_message='Registration successful'))
 
     return render_template('register.html', success_message=request.args.get('success_message'), error_message=request.args.get('error_message'))
+
+def detect_faces(image_data):
+    image_array = np.frombuffer(image_data, np.uint8)
+    image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+    
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    
+    return len(faces)
    
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         name = request.form['name']
-
+        image_data = request.form['image']
+        
         conn = get_db_connection()
         cursor = conn.cursor()
-
-        sql_check = "SELECT * FROM facevote WHERE name = %s and vote = 0"
-        cursor.execute(sql_check, (name,))
-        result = cursor.fetchone()
-
-        if result:
-            session['name'] = name
-            cursor.close()
-            conn.close()
-            return redirect(url_for('vote', success_message='Login successful'))
-        else:
-            return render_template('login.html', error='Invalid login')
-
+        
+        num_faces = detect_faces(image_data)
+        
+        if num_faces == 1:
+            sql_check = "SELECT * FROM facevote WHERE name = %s and vote = 0"
+            cursor.execute(sql_check, (name,))
+            result = cursor.fetchone()
+            
+            if result:
+                session['name'] = name
+                cursor.close()
+                conn.close()
+                return redirect(url_for('vote', success_message='Login successful'))
+        
+        return render_template('login.html', error='Invalid login')
+    
     return render_template('login.html')
    
 @app.route('/vote', methods=['GET', 'POST'])
