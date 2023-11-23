645588bcde4a2212565c1381242cc61668977499
locker55198 <150778940+locker55198@users.noreply.github.com>
2023-11-24 00:00:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import hashlib
 import numpy as np
 import cv2
 from flask import Flask, redirect, render_template, request, jsonify, send_from_directory, url_for, flash, session
-from connect import get_db_connection, login
+from connect import get_db_connection
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'fypfacevote'
