f71c9150c758c66dff51c8d5b476db02f7365751
locker55198 <150778940+locker55198@users.noreply.github.com>
2023-11-25 01:22:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,59 +10,6 @@ from deepface_detection import start_face_detection, is_face_matched
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'fypfacevote'
 
-def register_face(name, image_path):
-    cap = cv2.VideoCapture(0)
-
-    faceCascade = cv2.CascadeClassifier("models/haarcascade_frontalface_default.xml")
-
-    if not cap.isOpened():
-        print("Not Cam")
-        return
-
-    while True:
-        ret, frame = cap.read()
-
-        if not ret:
-            print("Not Image")
-            break
-
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        faces = faceCascade.detectMultiScale(
-            gray,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30, 30)
-        )
-
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-        cv2.imshow("Register", frame)
-
-        if cv2.waitKey(1) == ord('q'):
-            cv2.imwrite(image_path, frame)
-            break
-
-    cap.release()
-
-    cv2.destroyAllWindows()
-
-    try:
-        conn = mysql.connector.connect(**db_config)
-        cursor = conn.cursor()
-        query = "INSERT INTO facevote (name, image) VALUES (%s, %s)"
-        values = (name, image_path)
-        cursor.execute(query, values)
-        conn.commit()
-        print("Register Suggest {}！".format(name))
-    except mysql.connector.Error as error:
-        print("DataBase Error: {}".format(error))
-    finally:
-        if conn.is_connected():
-            cursor.close()
-            conn.close()
-
 @app.route('/')
 def index():
    print('Request for index page received')
