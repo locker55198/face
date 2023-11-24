d067b7b3d43ac8e8f32286e3a9616726d3a71772
locker55198 <150778940+locker55198@users.noreply.github.com>
2023-11-24 11:37:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import numpy as np
 import cv2
 from flask import Flask, redirect, render_template, request, jsonify, send_from_directory, url_for, flash, session
 from connect import get_db_connection
-from deepface_detection import start_face_detection, is_face_matched
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'fypfacevote'
