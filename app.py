0cf060fc754996d65e2303f960a512c244749e72
locker55198 <150778940+locker55198@users.noreply.github.com>
2023-11-24 20:02:37 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+from base64 import b64decode
+import numpy as np
+import cv2
+from flask import Flask, redirect, render_template, request, jsonify, send_from_directory, url_for, flash, session
+from connect import get_db_connection
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'fypfacevote'
+
+@app.route('/')
+def index():
+   print('Request for index page received')
+   return render_template('index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
---
+++ connect.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+def get_db_connection():
+    conn = mysql.connector.connect(
+        host='fyp.mysql.database.azure.com',
+        user='ming',
+        password='P@ssw0rd',
+        database='fyp'
+    )
+    return conn
+
+def update_vote(candidate):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("UPDATE facevote SET vote = %s", (candidate,))
+
+    conn.commit()
+    cursor.close()
+    conn.close()
