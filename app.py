b3d50a1d4101722503d572c305c75f1a26671648
locker55198 <150778940+locker55198@users.noreply.github.com>
2023-11-26 21:55:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-import cv2
 from flask import Flask, redirect, render_template, request, jsonify, send_from_directory, url_for, flash, session
 
 app = Flask(__name__)
@@ -14,6 +13,23 @@ def index():
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/vote', methods=['GET', 'POST'])
+def vote():
+    if request.method == 'POST':
+        candidate = request.form['vote']
+        name = session.get('name')
+        if name is None:
+            return redirect(url_for('login'))
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("UPDATE facevote SET vote = %s WHERE name = %s", (candidate, name))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return redirect(url_for('index', success_message='Vote successful'))
+    
+    return render_template('vote.html')
 
 if __name__ == '__main__':
     app.debug = True
