95ee883b56436207f5af7b50a46340f589236fbb
locker55198 <150778940+locker55198@users.noreply.github.com>
2023-11-26 20:38:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
 import os
-from base64 import b64decode
-import numpy as np
-import cv2
 from flask import Flask, redirect, render_template, request, jsonify, send_from_directory, url_for, flash, session
-from connect import get_db_connection
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'fypfacevote'
 
 @app.route('/')
 def index():
@@ -18,7 +13,6 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-   
 if __name__ == '__main__':
     app.debug = True
     app.run()
