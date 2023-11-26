d6b5b16762ac1320e9ff6e6d4eb6da9343dbadda
locker55198 <150778940+locker55198@users.noreply.github.com>
2023-11-26 23:52:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import hashlib
+import cv2
 from flask import Flask, redirect, render_template, request, jsonify, send_from_directory, url_for, flash, session
 from connect import get_db_connection
 from base64 import b64decode
