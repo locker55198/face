fac3a20b2e6ae546db93c5d1827d0d7fe7d278cf
locker55198 <150778940+locker55198@users.noreply.github.com>
2023-12-01 19:03:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,31 +21,44 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
+@app.route('/')
+def index():
+   print('Request for index page received')
+   return render_template('index.html')
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         name = request.form['name']
+        image_base64 = request.form['image']
 
-        ret, frame = camera.read()
+        # 將Base64編碼的圖像資料解碼為字節流
+        image_data = b64decode(image_base64)
 
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        conn = get_db_connection()
+        cursor = conn.cursor()
 
-        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+        sql_check = "SELECT * FROM facevote WHERE name = %s"
+        cursor.execute(sql_check, (name,))
+        result = cursor.fetchone()
 
-        if len(faces) > 0:
-            for (x, y, w, h) in faces:
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
-            _, img_encoded = cv2.imencode('.jpg', frame)
-            img_base64 = b64encode(img_encoded.tobytes()).decode('utf-8')
-
-            sql = "INSERT INTO facevote (name, image) VALUES (%s, %s)"
-            cursor.execute(sql, (name, img_base64))
-            conn.commit()
-
-            return redirect(url_for('index', success_message='Registration successful'))
+        if result:
+            cursor.close()
+            conn.close()
+            return redirect(url_for('register', error_message='Name already exists. Please choose a different name'))
         else:
-            return redirect(url_for('register', error_message='No face detected. Please try again.'))
+            sql = "INSERT INTO facevote (name, image) VALUES (%s, %s)"
+            cursor.execute(sql, (name, image_data))
+            conn.commit()
+            cursor.close()
+            conn.close()
+            return redirect(url_for('index', success_message='Registration successful'))
 
     return render_template('register.html', success_message=request.args.get('success_message'), error_message=request.args.get('error_message'))
 
@@ -53,48 +66,27 @@ def register():
 def login():
     if request.method == 'POST':
         name = request.form['name']
-        image_base64 = request.form['image']
 
-        image_data = np.frombuffer(b64decode(image_base64), np.uint8)
-        image = cv2.imdecode(image_data, cv2.IMREAD_COLOR)
-    
-        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-        # Add your face recognition logic here
-        # label, confidence = face_recognizer.predict(gray_image)
-        # conn = get_db_connection()
-        # cursor = conn.cursor()
-        # cursor.execute("SELECT * FROM facevote WHERE id=?", (label,))
-        # user = cursor.fetchone()
-
-        # if confidence < 100 and user is not None and user[1] == name:
-        #     session['name'] = name
-        #     cursor.close()
-        #     conn.close()
-        #     return redirect(url_for('vote', success_message='Login successful'))
-        # else:
-        #     cursor.close()
-        #     conn.close()
-        #     return render_template('login.html', error='Invalid login')
-
-        # For testing purposes, always redirect to the vote page
         conn = get_db_connection()
         cursor = conn.cursor()
-        sql_check = "SELECT * FROM facevote WHERE name = %s and vote = 0"
+
+        sql_check = "SELECT * FROM facevote WHERE name = %s"
         cursor.execute(sql_check, (name,))
         result = cursor.fetchone()
-        
+
         if result:
-            session['name'] = name
+            # 將使用者名稱存入session
+            #session['name'] = name
+
             cursor.close()
             conn.close()
-            return redirect(url_for('vote', success_message='Login successful'))
+            return redirect(url_for('vote'))
         else:
             cursor.close()
             conn.close()
-            return render_template('login.html', error='Invalid login')
-    
-    return render_template('login.html')
+            return redirect(url_for('login', error_message='Invalid name. Please try again.'))
+
+    return render_template('login.html', error_message=request.args.get('error_message'))
 
 @app.route('/vote', methods=['GET', 'POST'])
 def vote():
@@ -110,9 +102,10 @@ def vote():
         cursor.close()
         conn.close()
         return redirect(url_for('index', success_message='Vote successful'))
-    
+
     return render_template('vote.html')
 
 if __name__ == '__main__':
     app.debug = True
     app.run()
+
